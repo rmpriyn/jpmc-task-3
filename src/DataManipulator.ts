067b63c07ed01c856b21425c3ef0f9ab0de5f8eb
rmpriyn <64906530+rmpriyn@users.noreply.github.com>
2023-01-03 18:14:08 +0530

    Update DataManipulator.ts

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -11,12 +11,17 @@ export interface Row {
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+  static generateRow(serverResponds: ServerRespond[]):row {
+   const priceABC - (serverRespond [0]. top_ask.price + serverRespond[0].top bid.price) / 2;
+const priceDEF = (serverRespond [1] .top ask.price + serverRespond[1].top bid.price) / 2;
+const ratio = priceABC / priceDEF;
+const upperBound = 1 + 0.05;
+const lowerBound = 1 - 0.05;
+return {
+price _abc: priceABC, price def: priceDEF, ratio,
+timestamp: serverRespond[©] .timestamp > serverRespond [1]. timestamp ? serverRespond[®].timestamp: serverRespond [1]. timestamp, upper_bound: upperBound,
+Lower bound: lowerBound,
+trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio: undefined,
       };
     })
   }
